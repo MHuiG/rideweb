101246e674df66c37500b3844285fccefb6d2d92
MHuiG <616001163@qq.com>
2020-01-03 16:45:13 +0800

    commit

---
+++ src/main/java/cn/controller/CassandraController.java
@@ -0,0 +1,26 @@
+package cn.controller;
+
+import cn.service.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class CassandraController {
+
+
+    @Autowired
+    private Service Service;
+
+    @RequestMapping(value = "/getRatingSum", produces = "application/json;charset=UTF-8")
+    @ResponseBody
+    public String getRatingSum() {
+        System.out.println("getRatingSum..");
+        String data = Service.getRatingSum(5);
+        return data;
+    }
+
+
+}
