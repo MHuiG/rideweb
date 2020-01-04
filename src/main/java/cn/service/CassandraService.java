63fc3b9366b19ba2d560d96a402cd6eedfcb26db
MHuiG <616001163@qq.com>
2020-01-04 16:19:38 +0800

    commit

--- src/main/java/cn/service/CassandraService.java
+++ src/main/java/cn/service/CassandraService.java
@@ -1,5 +1,15 @@
 package cn.service;
 
+import cn.pojo.Location;
+import com.datastax.driver.core.Session;
+
 public interface CassandraService {
+    void connectcass(Session session);
+
+    void insertLocation(Session session, Location o);
+
+    void deletecass(Session session);
+
+    void querycass(Session session);
 
 }
