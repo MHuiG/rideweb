4640a599cc48fb8b12b6a0e44a5814aa543d8273
ywhandzmx <964813881@qq.com>
2020-01-05 20:15:19 +0800

    11

--- src/main/java/cn/service/impl/MongoDBServiceImpl.java
+++ src/main/java/cn/service/impl/MongoDBServiceImpl.java
@@ -2,34 +2,26 @@ package cn.service.impl;
 
 import cn.mapper.Mapper;
 import cn.service.MongoDBService;
-import org.aspectj.lang.annotation.Before;
-import org.springframework.beans.factory.annotation.Autowired;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
-import com.mongodb.Mongo;
-import com.mongodb.MongoException;
-import com.mongodb.BasicDBObject;
-import com.mongodb.Block;
-import com.mongodb.ConnectionString;
-import com.mongodb.client.FindIterable;
+import com.mongodb.*;
+import com.mongodb.client.*;
 import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoDatabase;
+import org.springframework.beans.factory.annotation.Autowired;
 import com.mongodb.client.gridfs.GridFSBucket;
 import com.mongodb.client.gridfs.GridFSBuckets;
 import com.mongodb.client.gridfs.model.GridFSUploadOptions;
-import com.mongodb.client.MongoCollection;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.bson.Document;
 import org.bson.types.ObjectId;
 
-import com.mongodb.client.MongoCursor;
 import static com.mongodb.client.model.Updates.*;
 import static com.mongodb.client.model.Filters.*;
 import com.mongodb.client.gridfs.*;
@@ -45,11 +37,25 @@ public class MongoDBServiceImpl implements MongoDBService {
     public void main(){
         MongoClient mongo = MongoClients.create("mongodb://worker02:27017");
         MongoDatabase db = mongo.getDatabase("ywh");
-        MongoCollection col = db.getCollection("location");
+        MongoCollection col = db.getCollection("season");
 
-        MongoCursor<Document> cursor = col.find().iterator();
-        System.out.println("success!!!");
+        BasicDBObject searchQuery = new BasicDBObject();
+        DBObject matchBasicDBObjet = new BasicDBObject("$match",new BasicDBObject("Account type","Casual"));
+        DBObject groupFields = new BasicDBObject("_id",new BasicDBObject("Start station","$Start station"));
+        groupFields.put("num",new BasicDBObject("$sum",1));
+        DBObject group = new BasicDBObject("$group", groupFields);
+        DBObject sortchBasicDBObjet = new BasicDBObject("$sort", new BasicDBObject("count",-1));
+        DBObject limitBasicDBObjet = new BasicDBObject("$limit",3);
+        List<DBObject> list = new ArrayList<DBObject>();
+        list.add(matchBasicDBObjet);
+        list.add(group);
+        list.add(sortchBasicDBObjet);
+        list.add(limitBasicDBObjet);
+        System.out.println(col.aggregate(list).toString());
+
+//      System.out.println("success!!!");
     }
+
 
 
 
