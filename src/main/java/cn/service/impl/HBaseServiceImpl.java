a8f1f714ddd37e1d7fcad669835f63f39870af9d
nijiagebilaowang <565928371@qq.com>
2020-01-06 19:23:09 +0800

    1

--- src/main/java/cn/service/impl/HBaseServiceImpl.java
+++ src/main/java/cn/service/impl/HBaseServiceImpl.java
@@ -2,6 +2,7 @@ package cn.service.impl;
 
 import cn.mapper.Mapper;
 import cn.pojo.Cassandra.Location;
+import cn.pojo.Cassandra.Season;
 import cn.service.HBaseService;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.*;
@@ -88,88 +89,129 @@ public class HBaseServiceImpl implements HBaseService {
         }
     }
 // 查询整表
-
-    public void queryTable(String tableName) throws IOException {
-        try{
+    @Test
+    public List<Season> queryTable() throws IOException {
             getconncet();
-            List<Location> p = new ArrayList<>();
-            Table table =connection.getTable(TableName.valueOf(tableName));
+            List<Season> p = new ArrayList<>();
+            Table table =connection.getTable(TableName.valueOf("season"));
             Scan scan=new Scan();
             ResultScanner results=table.getScanner(scan);
             for (Result result:results){
-                Location o = new Location();
-                System.out.println(new String(result.getRow()));
-                o.setTerminal(new String(result.getRow()));
+                Season o = new Season();
+//                System.out.println(new String(result.getRow()));
+                o.setId(new String(result.getRow()));
                 for (Cell cell:result.rawCells()){
                     //列名
-                    if (new String(CellUtil.cloneQualifier(cell)).equals("Latitude")){
-                        o.setLatitude(new String(CellUtil.cloneValue(cell)));
+                    if (new String(CellUtil.cloneQualifier(cell)).equals("Startdate")){
+                        o.setStartDate(new String(CellUtil.cloneValue(cell)));
 //                        System.out.println(new String(CellUtil.cloneValue(cell)));
                     }
-                    if(new String(CellUtil.cloneQualifier(cell)).equals("Longitude")){
-                        o.setLongitude(new String(CellUtil.cloneValue(cell)));
+                    if(new String(CellUtil.cloneQualifier(cell)).equals("Startstation")){
+                        o.setStartStation(new String(CellUtil.cloneValue(cell)));
 //                        System.out.println(new String(CellUtil.cloneValue(cell)));
                     }
-                    if(new String(CellUtil.cloneQualifier(cell)).equals("Station")){
-                        o.setStation(new String(CellUtil.cloneValue(cell)));
+                    if(new String(CellUtil.cloneQualifier(cell)).equals("Startterminal")){
+                        o.setStartStationNumber(new String(CellUtil.cloneValue(cell)));
 //                        System.out.println(new String(CellUtil.cloneValue(cell)));
                     }
-                    if(new String(CellUtil.cloneQualifier(cell)).equals("Nbdocks")){
-                        o.setNbdocks(new String(CellUtil.cloneValue(cell)));
+                    if(new String(CellUtil.cloneQualifier(cell)).equals("Enddate")){
+                        o.setEndDate(new String(CellUtil.cloneValue(cell)));
+//                        System.out.println(new String(CellUtil.cloneValue(cell)));
+                    }
+                    if (new String(CellUtil.cloneQualifier(cell)).equals("Endstation")){
+                        o.setEndStation(new String(CellUtil.cloneValue(cell)));
+//                        System.out.println(new String(CellUtil.cloneValue(cell)));
+                    }
+                    if (new String(CellUtil.cloneQualifier(cell)).equals("Endterminal")){
+                        o.setEndStationNumber(new String(CellUtil.cloneValue(cell)));
+//                        System.out.println(new String(CellUtil.cloneValue(cell)));
+                    }
+                    if (new String(CellUtil.cloneQualifier(cell)).equals("Totalduration")){
+                        o.setTotalDuration(new String(CellUtil.cloneValue(cell)));
+//                        System.out.println(new String(CellUtil.cloneValue(cell)));
+                    }
+                    if (new String(CellUtil.cloneQualifier(cell)).equals("Accounttype")){
+                        o.setAccountType(new String(CellUtil.cloneValue(cell)));
 //                        System.out.println(new String(CellUtil.cloneValue(cell)));
                     }
                 }
-                System.out.println("**************************");
-                System.out.println(""+o.getTerminal()+o.getStation()+o.getLatitude()+o.getLongitude()+o.getNbdocks());
-                System.out.println("**************************");
+//                System.out.println(o.getTerminal()+" "+o.getStation()+" "+o.getLatitude()+" "+o.getLongitude()+" "+o.getNbdocks());
                 p.add(o);
             }
-
-            System.out.println(p.toString());
-         System.out.println("---------------查询整表数据结束----------");
-        }catch (IOException e) {
-                         e.printStackTrace();
-                     System.out.println("查询失败");
-            }
+            return p;
     }
 
-    //get查询数据
+    //根据行键get查询数据
     @Test
-    public void getData(String tableName, String row) throws IOException {
-        try {
+    public List<Season> getData(Season s) throws IOException {
             getconncet();
-            Table table_Name = connection.getTable(TableName.valueOf(tableName));
-            Get get1 = new Get(Bytes.toBytes(row));
+            List<Season> p = new ArrayList<>();
+            Table table_Name = connection.getTable(TableName.valueOf("season"));
+            Get get1 = new Get(Bytes.toBytes(s.getId()));
             Result result = table_Name.get(get1);
+            Season o = new Season();
+            o.setId(new String(result.getRow()));
+            System.out.println(new String(result.getRow()));
             for (Cell cell : result.rawCells()) {
-                //分别是cell、列族名、列名、值、键值对的时间戳
-                System.out.println("cell是"+new String(CellUtil.getCellKeyAsString(cell)) + " \t"+"列族名是" + new String(CellUtil.cloneFamily(cell))
-                        + " \t"+"列名是" + new String(CellUtil.cloneQualifier(cell)) + " \t"+"值和时间戳分别是" + new String(CellUtil.cloneValue(cell)) + ":" + cell.getTimestamp());
-
+                if (new String(CellUtil.cloneQualifier(cell)).equals("Startdate")){
+                    o.setStartDate(new String(CellUtil.cloneValue(cell)));
+                        System.out.println(new String(CellUtil.cloneValue(cell)));
+                }
+                if(new String(CellUtil.cloneQualifier(cell)).equals("Startstation")){
+                    o.setStartStation(new String(CellUtil.cloneValue(cell)));
+                        System.out.println(new String(CellUtil.cloneValue(cell)));
+                }
+                if(new String(CellUtil.cloneQualifier(cell)).equals("Startterminal")){
+                    o.setStartStationNumber(new String(CellUtil.cloneValue(cell)));
+                        System.out.println(new String(CellUtil.cloneValue(cell)));
+                }
+                if(new String(CellUtil.cloneQualifier(cell)).equals("Enddate")){
+                    o.setEndDate(new String(CellUtil.cloneValue(cell)));
+                        System.out.println(new String(CellUtil.cloneValue(cell)));
+                }
+                if (new String(CellUtil.cloneQualifier(cell)).equals("Endstation")){
+                    o.setEndStation(new String(CellUtil.cloneValue(cell)));
+                    System.out.println(new String(CellUtil.cloneValue(cell)));
+                }
+                if (new String(CellUtil.cloneQualifier(cell)).equals("Endterminal")){
+                    o.setEndStationNumber(new String(CellUtil.cloneValue(cell)));
+                    System.out.println(new String(CellUtil.cloneValue(cell)));
+                }
+                if (new String(CellUtil.cloneQualifier(cell)).equals("Totalduration")){
+                    o.setTotalDuration(new String(CellUtil.cloneValue(cell)));
+                    System.out.println(new String(CellUtil.cloneValue(cell)));
+                }
+                if (new String(CellUtil.cloneQualifier(cell)).equals("Accounttype")){
+                    o.setAccountType(new String(CellUtil.cloneValue(cell)));
+                    System.out.println(new String(CellUtil.cloneValue(cell)));
+                }
             }
+//            System.out.println(o.getId()+" "+o.getStartDate()+" "+o.getStartStation()+" "+o.getStartStationNumber()+" "+o.getEndDate()+" "+o.getEndStation()+" "+o.getEndStationNumber()+" "+o.getTotalDuration()+" "+o.getAccountType());
+            p.add(o);
             table_Name.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("get方法查询表失败");
-        }
+            return p;
     }
 //    插入数据
     @Test
-    public void addData(String tableName,String Terminal,String Station,String Latitude,String Longitude,String Nbdocks) throws IOException {
+    public void addSeasonData(Season o) throws IOException {
         try {
             getconncet();
-            HTable table_Name = (HTable) connection.getTable(TableName.valueOf(tableName));
+            HTable table_Name = (HTable) connection.getTable(TableName.valueOf("ride"));
             //设置客户端缓存大小，即缓存为6MB时执行真正的put，在flushCommits时进行手动提交。若为true，则每次put操作都直接提交给regionserver，在大批量写入时效率较低。
             table_Name.setWriteBufferSize(6 * 1024 * 1024);
             table_Name.setAutoFlushTo(false);
             //关闭WAL写入
-            Put put1 = new Put(Bytes.toBytes(Terminal)); //行键
+            Put put1 = new Put(Bytes.toBytes(o.getId())); //行键
             put1.setDurability(Durability.SKIP_WAL); //批量加载，关闭预写日志WAL，如果出现问题，可以重新运行批量负载，而不会有数据丢失的风险
             //列族名，列名，值，以下数据的时间戳是相同的
-            put1.addColumn(Bytes.toBytes("info"), Bytes.toBytes("Latitude"), Bytes.toBytes(Latitude));
-            put1.addColumn(Bytes.toBytes("info"), Bytes.toBytes("Longitude"), Bytes.toBytes(Longitude));
-            put1.addColumn(Bytes.toBytes("info"), Bytes.toBytes("Station"), Bytes.toBytes(Station));
-            put1.addColumn(Bytes.toBytes("info"), Bytes.toBytes("Nbdocks"), Bytes.toBytes(Nbdocks));
+            put1.addColumn(Bytes.toBytes("info"), Bytes.toBytes("Startdate"), Bytes.toBytes(o.getStartDate()));
+            put1.addColumn(Bytes.toBytes("info"), Bytes.toBytes("Startstation"), Bytes.toBytes(o.getStartStation()));
+            put1.addColumn(Bytes.toBytes("info"), Bytes.toBytes("Startterminal"), Bytes.toBytes(o.getStartStationNumber()));
+            put1.addColumn(Bytes.toBytes("info"), Bytes.toBytes("Enddate"), Bytes.toBytes(o.getEndDate()));
+            put1.addColumn(Bytes.toBytes("info"), Bytes.toBytes("Endstation"), Bytes.toBytes(o.getEndStation()));
+            put1.addColumn(Bytes.toBytes("info"), Bytes.toBytes("Endterminal"), Bytes.toBytes(o.getEndStationNumber()));
+            put1.addColumn(Bytes.toBytes("info"), Bytes.toBytes("Totalduration"), Bytes.toBytes(o.getTotalDuration()));
+            put1.addColumn(Bytes.toBytes("info"), Bytes.toBytes("Accounttype"), Bytes.toBytes(o.getAccountType()));
             table_Name.put(put1);
             table_Name.flushCommits();
             System.out.println("添加数据成功");
@@ -181,10 +223,10 @@ public class HBaseServiceImpl implements HBaseService {
     }
 //    删除行数据
     @Test
-    public void deleteRow(String tableName, String... rows) throws IOException {
+    public void deleteRow(String... rows) throws IOException {
         try {
             getconncet();
-            TableName table_Name = TableName.valueOf(tableName);
+            TableName table_Name = TableName.valueOf("season");
             HTable table = (HTable) connection.getTable(table_Name);
             for (int i = 0; i < rows.length; i++) {
                 Delete delete = new Delete(Bytes.toBytes(rows[i]));
@@ -197,37 +239,19 @@ public class HBaseServiceImpl implements HBaseService {
             System.out.println("删除行失败");
         }
     }
-//    修改数据
-    @Test
-    public void updatedata(String tableName,String Terminal,String row,String data) throws IOException{
-        try{
-            getconncet();
-            HTable table_Name = (HTable) connection.getTable(TableName.valueOf(tableName));
-            //设置客户端缓存大小，即缓存为6MB时执行真正的put，在flushCommits时进行手动提交。若为true，则每次put操作都直接提交给regionserver，在大批量写入时效率较低。
-            table_Name.setWriteBufferSize(6 * 1024 * 1024);
-            table_Name.setAutoFlushTo(false);
-            Put put1 = new Put(Bytes.toBytes(Terminal));
-            //Updating
-            put1.addColumn(Bytes.toBytes("info"), Bytes.toBytes(row),Bytes.toBytes(data));
-            //save
-            table_Name.put(put1);
-            table_Name.flushCommits();
-            System.out.println("data Updated!");
-            table_Name.close();
-        }catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("修改行失败");
-        }
-    }
 
     @Test
     public void hh(){
         try{
+            deleteTable("season");
+            createTable("season","info");
 //            deleteRow("locations","4000") ;
 //            addData("locations","4000","wfc","400","500","50");
 //            updatedata("locations","4000","Station","www");
 //            getData("locations","4000");
-            queryTable("locations");
+//            queryTable("locations");
+//              getData("locations","30000");
+//            addSeasonData("season","3000000",)
         }catch (IOException e){
             e.printStackTrace();
         }
