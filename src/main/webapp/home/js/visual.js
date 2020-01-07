bb2fa6186d385d2cb138dcae1e32983a2db17d96
MHuiG <616001163@qq.com>
2020-01-07 09:11:18 +0800

    commit

--- src/main/webapp/home/js/visual.js
+++ src/main/webapp/home/js/visual.js
@@ -236,137 +236,6 @@ option1 = {
     },
     series: myData.seriesArr
 }
-//////////////////////交通流量 end
-
-//交通工具流量
-// option2 = {
-//
-//     tooltip: {//鼠标指上时的标线
-//         trigger: 'axis',
-//         axisPointer: {
-//             lineStyle: {
-//                 color: '#fff'
-//             }
-//         }
-//     },
-//     legend: {
-//         icon: 'rect',
-//         itemWidth: 14,
-//         itemHeight: 5,
-//         itemGap: 13,
-//         data: ['会员车', '临时车'],
-//         right: '10px',
-//         top: '0px',
-//         textStyle: {
-//             fontSize: 12,
-//             color: '#fff'
-//         }
-//     },
-//     grid: {
-//         x: 35,
-//         y: 25,
-//         x2: 8,
-//         y2: 25,
-//     },
-//     xAxis: [{
-//         type: 'category',
-//         boundaryGap: false,
-//         axisLine: {
-//             lineStyle: {
-//                 color: '#57617B'
-//             }
-//         },
-//         axisLabel: {
-//             textStyle: {
-//                 color: '#fff',
-//             },
-//         },
-//         data: ['2010', '2010', '2011', '2011', '2012', '2012', '2013', '2013', '2014', '2014', '2015', '2015']
-//     }],
-//     yAxis: [{
-//         type: 'value',
-//         axisTick: {
-//             show: false
-//         },
-//         axisLine: {
-//             lineStyle: {
-//                 color: '#57617B'
-//             }
-//         },
-//         axisLabel: {
-//             margin: 10,
-//             textStyle: {
-//                 fontSize: 14
-//             },
-//             textStyle: {
-//                 color: '#fff',
-//             },
-//         },
-//         splitLine: {
-//             lineStyle: {
-//                 color: '#57617B'
-//             }
-//         }
-//     }],
-//     series: [{
-//         name: '会员车',
-//         type: 'line',
-//         smooth: true,
-//         lineStyle: {
-//             normal: {
-//                 width: 2
-//             }
-//         },
-//         areaStyle: {
-//             normal: {
-//                 color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
-//                     offset: 0,
-//                     color: 'rgba(137, 189, 27, 0.3)'
-//                 }, {
-//                     offset: 0.8,
-//                     color: 'rgba(137, 189, 27, 0)'
-//                 }], false),
-//                 shadowColor: 'rgba(0, 0, 0, 0.1)',
-//                 shadowBlur: 10
-//             }
-//         },
-//         itemStyle: {
-//             normal: {
-//                 color: 'rgb(137,189,27)'
-//             }
-//         },
-//         data: [20, 35, 34, 45, 52, 41, 49, 64, 24, 52.4, 24, 33]
-//     }, {
-//         name: '临时车',
-//         type: 'line',
-//         smooth: true,
-//         lineStyle: {
-//             normal: {
-//                 width: 2
-//             }
-//         },
-//         areaStyle: {
-//             normal: {
-//                 color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
-//                     offset: 0,
-//                     color: 'rgba(0, 136, 212, 0.3)'
-//                 }, {
-//                     offset: 0.8,
-//                     color: 'rgba(0, 136, 212, 0)'
-//                 }], false),
-//                 shadowColor: 'rgba(0, 0, 0, 0.1)',
-//                 shadowBlur: 10
-//             }
-//         },
-//         itemStyle: {
-//             normal: {
-//                 color: 'rgb(0,136,212)'
-//             }
-//         },
-//         data: [97.3, 99.2, 99.3, 100.0, 99.6, 90.6, 80.0, 91.5, 69.8, 67.5, 90.4, 84.9]
-//     },]
-// };
-//////////////////////交通工具流量 end
 
 //本月发生事件1
 var color = ['#e9df3d', '#f79c19', '#21fcd6', '#08c8ff', '#df4131'];
@@ -424,7 +293,7 @@ var renderData = [{
 
 
 data.forEach(function (d, i) {
-    var value = ['', '', '', '', ''];
+    var value = [];
     value[i] = max,
         renderData[0].value[i] = d.value;
     renderData.push({
@@ -498,6 +367,7 @@ option3 = {
 }
 //////////////////////本月发生事件1 end
 //本月发生事件2
+
 var color = ['#e9df3d', '#f79c19', '#21fcd6', '#08c8ff', '#df4131'];
 var data = [{
     "name": "车锁",
@@ -1219,6 +1089,137 @@ option7 = {
 }
 //////////////////////今日实时收费 end
 
+//////////////////////交通流量 end
+
+//交通工具流量
+// option2 = {
+//
+//     tooltip: {//鼠标指上时的标线
+//         trigger: 'axis',
+//         axisPointer: {
+//             lineStyle: {
+//                 color: '#fff'
+//             }
+//         }
+//     },
+//     legend: {
+//         icon: 'rect',
+//         itemWidth: 14,
+//         itemHeight: 5,
+//         itemGap: 13,
+//         data: ['会员车', '临时车'],
+//         right: '10px',
+//         top: '0px',
+//         textStyle: {
+//             fontSize: 12,
+//             color: '#fff'
+//         }
+//     },
+//     grid: {
+//         x: 35,
+//         y: 25,
+//         x2: 8,
+//         y2: 25,
+//     },
+//     xAxis: [{
+//         type: 'category',
+//         boundaryGap: false,
+//         axisLine: {
+//             lineStyle: {
+//                 color: '#57617B'
+//             }
+//         },
+//         axisLabel: {
+//             textStyle: {
+//                 color: '#fff',
+//             },
+//         },
+//         data: ['2010', '2010', '2011', '2011', '2012', '2012', '2013', '2013', '2014', '2014', '2015', '2015']
+//     }],
+//     yAxis: [{
+//         type: 'value',
+//         axisTick: {
+//             show: false
+//         },
+//         axisLine: {
+//             lineStyle: {
+//                 color: '#57617B'
+//             }
+//         },
+//         axisLabel: {
+//             margin: 10,
+//             textStyle: {
+//                 fontSize: 14
+//             },
+//             textStyle: {
+//                 color: '#fff',
+//             },
+//         },
+//         splitLine: {
+//             lineStyle: {
+//                 color: '#57617B'
+//             }
+//         }
+//     }],
+//     series: [{
+//         name: '会员车',
+//         type: 'line',
+//         smooth: true,
+//         lineStyle: {
+//             normal: {
+//                 width: 2
+//             }
+//         },
+//         areaStyle: {
+//             normal: {
+//                 color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+//                     offset: 0,
+//                     color: 'rgba(137, 189, 27, 0.3)'
+//                 }, {
+//                     offset: 0.8,
+//                     color: 'rgba(137, 189, 27, 0)'
+//                 }], false),
+//                 shadowColor: 'rgba(0, 0, 0, 0.1)',
+//                 shadowBlur: 10
+//             }
+//         },
+//         itemStyle: {
+//             normal: {
+//                 color: 'rgb(137,189,27)'
+//             }
+//         },
+//         data: [20, 35, 34, 45, 52, 41, 49, 64, 24, 52.4, 24, 33]
+//     }, {
+//         name: '临时车',
+//         type: 'line',
+//         smooth: true,
+//         lineStyle: {
+//             normal: {
+//                 width: 2
+//             }
+//         },
+//         areaStyle: {
+//             normal: {
+//                 color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+//                     offset: 0,
+//                     color: 'rgba(0, 136, 212, 0.3)'
+//                 }, {
+//                     offset: 0.8,
+//                     color: 'rgba(0, 136, 212, 0)'
+//                 }], false),
+//                 shadowColor: 'rgba(0, 0, 0, 0.1)',
+//                 shadowBlur: 10
+//             }
+//         },
+//         itemStyle: {
+//             normal: {
+//                 color: 'rgb(0,136,212)'
+//             }
+//         },
+//         data: [97.3, 99.2, 99.3, 100.0, 99.6, 90.6, 80.0, 91.5, 69.8, 67.5, 90.4, 84.9]
+//     },]
+// };
+//////////////////////交通工具流量 end
 
 
 
