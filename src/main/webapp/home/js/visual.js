f9198b9e5b4ca099c4e388f663079939f1f26c3f
MHuiG <616001163@qq.com>
2020-01-08 11:27:43 +0800

    commit

--- src/main/webapp/home/js/visual.js
+++ src/main/webapp/home/js/visual.js
@@ -14,21 +14,241 @@ $.ajax({
     type: "POST",
     dataType: "json",
     success: function (data) {
-        options = {
-            yAxis:[
-                [data[0][0],data[0][1],data[0][2],data[0][3],data[0][4],data[0][5],data[0][6]],
-                [data[1][0],data[1][1],data[1][2],data[1][3],data[1][4],data[1][5],data[1][6]]
+
+        data = {
+            id: 'multipleBarsLines',
+            barColor: ['#009883', '#e66922'], //柱子颜色 必填参数
+            legendBar: ['<=15min', '>15min'],
+            xAxis: ['2010', '2011', '2012', '2013', '2014', '2015', '2016'],
+            yAxis: [
+                [data[0][0], data[0][1], data[0][2], data[0][3], data[0][4], data[0][5], data[0][6]],
+                [data[1][0], data[1][1], data[1][2], data[1][3], data[1][4], data[1][5], data[1][6]]
             ]
         };
         // console.log(data);
-        var option = myChart1.getOption();
-        option.series[0].data.yAxis = options.yAxis;
-        myChart1.setOption(option);
-        //myChart1.setOption(option1);
+        myData = (function test() {
+            let yAxis = data.yAxis || [];
+            let lines = data.lines || [];
+            let legendBar = data.legendBar || [];
+            let legendLine = data.legendLine || [];
+            var symbol = data.symbol || ' ';
+            let seriesArr = [];
+            let legendArr = [];
+            yAxis && yAxis.forEach((item, index) => {
+                legendArr.push({
+                    name: legendBar && legendBar.length > 0 && legendBar[index]
+                });
+                seriesArr.push({
+                    name: legendBar && legendBar.length > 0 && legendBar[index],
+                    type: 'bar',
+                    barGap: '0.5px',
+                    data: item,
+                    barWidth: data.barWidth || 12,
+                    label: {
+                        normal: {
+                            show: false,
+                            formatter: '{c}' + symbol,
+                            position: 'top',
+                            textStyle: {
+                                color: '#000',
+                                fontStyle: 'normal',
+                                fontFamily: '微软雅黑',
+                                textAlign: 'left',
+                                fontSize: 11,
+                            },
+                        },
+                    },
+                    itemStyle: { //图形样式
+                        normal: {
+                            barBorderRadius: 0,
+                            borderWidth: 1,
+                            borderColor: '#ddd',
+                            color: data.barColor[index]
+                        },
+                    }
+                })
+            });
+
+            lines && lines.forEach((item, index) => {
+                    legendArr.push({
+                        name: legendLine && legendLine.length > 0 && legendLine[index]
+                    });
+                    seriesArr.push({
+                        name: legendLine && legendLine.length > 0 && legendLine[index],
+                        type: 'line',
+                        data: item,
+                        itemStyle: {
+                            normal: {
+                                color: data.lineColor[index],
+                                lineStyle: {
+                                    width: 2,//折线宽度
+                                    type: 'solid',
+                                }
+                            }
+                        },
+                        label: {
+                            normal: {
+                                show: false, //折线上方label控制显示隐藏
+                                position: 'top',
+                            }
+                        },
+                        symbol: 'circle',
+                        symbolSize: 5
+                    })
+                }
+            );
+
+            return {
+                seriesArr,
+                legendArr
+            }
+        })();
+        option1 = {
+            title: {
+                show: true,
+                text: data.title,
+                subtext: data.subTitle,
+                link: '1111'
+            },
+            tooltip: {
+                trigger: 'axis',
+                formatter: function (params) {
+                    var time = '';
+                    var str = '';
+                    for (var i of params) {
+                        time = i.name.replace(/\n/g, '') + '<br/>';
+                        if (i.data == 'null' || i.data == null) {
+                            str += i.seriesName + '：无数据' + '<br/>'
+                        } else {
+                            str += i.seriesName + '：' + i.data + '<br/>'
+                        }
+                        ;
+
+                    }
+                    return time + str;
+                },
+                axisPointer: {
+                    type: 'none'
+                },
+            },
+            legend: {
+                right: data.legendRight || '30%',
+                top: 0,
+                right: 10,
+                itemGap: 16,
+                itemWidth: 10,
+                itemHeight: 10,
+                data: myData.legendArr,
+                textStyle: {
+                    color: '#fff',
+                    fontStyle: 'normal',
+                    fontFamily: '微软雅黑',
+                    fontSize: 12,
+                }
+            },
+            grid: {
+                x: 0,
+                y: 30,
+                x2: 0,
+                y2: 25,
+            },
+            xAxis: {
+                type: 'category',
+                data: data.xAxis,
+                axisTick: {
+                    show: false,
+                },
+
+                axisLine: {
+                    show: false,
+                },
+                axisLabel: {       //轴标
+                    show: true,
+                    interval: '0',
+                    textStyle: {
+                        lineHeight: 5,
+                        padding: [2, 2, 0, 2],
+                        height: 50,
+                        fontSize: 12,
+                        color: '#fff',
+                    },
+                    rich: {
+                        Sunny: {
+                            height: 50,
+                            width: 60,
+                            padding: [0, 5, 0, 5],
+                            align: 'center',
+                        },
+                    },
+                    formatter: function (params, index) {
+                        var newParamsName = "";
+                        var splitNumber = 7;
+                        var paramsNameNumber = params && params.length;
+                        if (paramsNameNumber && paramsNameNumber <= 4) {
+                            splitNumber = 4;
+                        } else if (paramsNameNumber >= 5 && paramsNameNumber <= 7) {
+                            splitNumber = 4;
+                        } else if (paramsNameNumber >= 8 && paramsNameNumber <= 9) {
+                            splitNumber = 5;
+                        } else if (paramsNameNumber >= 10 && paramsNameNumber <= 14) {
+                            splitNumber = 5;
+                        } else {
+                            params = params && params.slice(0, 15);
+                        }
+                        var provideNumber = splitNumber; //一行显示几个字
+                        var rowNumber = Math.ceil(paramsNameNumber / provideNumber) || 0;
+                        if (paramsNameNumber > provideNumber) {
+                            for (var p = 0; p < rowNumber; p++) {
+                                var tempStr = "";
+                                var start = p * provideNumber;
+                                var end = start + provideNumber;
+                                if (p == rowNumber - 1) {
+                                    tempStr = params.substring(start, paramsNameNumber);
+                                } else {
+                                    tempStr = params.substring(start, end) + "\n";
+                                }
+                                newParamsName += tempStr;
+                            }
+
+                        } else {
+                            newParamsName = params;
+                        }
+                        params = newParamsName;
+                        return '{Sunny|' + params + '}';
+                    },
+                    color: '#687284',
+                },
+
+            },
+            yAxis: {
+                axisLine: {
+                    show: false
+                },
+                axisTick: {
+                    show: false
+                },
+                axisLabel: {
+                    show: false
+                },
+                splitLine: {
+                    show: true,
+                    lineStyle: {
+                        color: '#F1F3F5',
+                        type: 'solid'
+                    },
+                    interval: 2
+                },
+                splitNumber: 4,
+            },
+            series: myData.seriesArr
+        };
+        myChart1.setOption(option1);
     },
-    error: function (err) {
-        console.log(err);
-    }
+    error:
+
+        function (err) {
+            console.log(err);
+        }
 });
 var myData = (function test() {
     let yAxis = data.yAxis || [];
