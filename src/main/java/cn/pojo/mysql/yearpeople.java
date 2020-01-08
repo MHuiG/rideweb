e4d0861d16b71debc0c730a54edaf8458e91b962
nijiagebilaowang <565928371@qq.com>
2020-01-08 10:07:11 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/cn/controller/HBaseController.java
    #	src/main/java/cn/service/impl/HBaseServiceImpl.java

---
+++ src/main/java/cn/pojo/mysql/yearpeople.java
@@ -0,0 +1,31 @@
+package cn.pojo.mysql;
+
+public class yearpeople {
+    public String year;
+    public String high;
+    public String low;
+
+    public String getYear() {
+        return year;
+    }
+
+    public String getHigh() {
+        return high;
+    }
+
+    public String getLow() {
+        return low;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public void setHigh(String high) {
+        this.high = high;
+    }
+
+    public void setLow(String low) {
+        this.low = low;
+    }
+}
